c38f6bc3974f17e831f50b1d37f7f490699e0180
Anthony Humphreys <53506488+AnthonyJamez12@users.noreply.github.com>
2021-12-20 13:42:58 -0600

    First Commit

---
+++ register/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+def register(request):
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        new_user = form.save()
+        messages.info(request, "Thanks for registering. You are now logged in.")
+        new_user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+        login(request, new_user)
+        return HttpResponseRedirect("/")
+    else:
+        form = RegisterForm()
+
+
+    return render(request, "register/register.html", {"form": form})
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/register')
